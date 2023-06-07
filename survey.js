7d7245bbc758a773b42d80eccfa3547f8b921643
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-06-06 22:10:37 -0400

    profileGenerator and surveyQuestions complete

---
+++ survey.js
@@ -0,0 +1,48 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const questions = [
+  "What's your name? Nicknames are also acceptable :)",
+  "What's an activity you like doing?",
+  "What do you listen to while doing that?",
+  "Which meal is your favourite (eg: dinner, brunch, etc.)?",
+  "What's your favourite thing to eat for that meal?",
+  "Which sport is your absolute favourite?",
+  "What is your superpower? In a few words, tell us what you are amazing at!"
+]
+
+
+let answers = []
+
+const askQuestion = (questionIndex) => {
+  if (questionIndex === questions.length) {
+    rl.close();
+    generateProfile();
+    return;
+  }
+
+  rl.question(questions[questionIndex] + ' ', (answer) => {
+    answers.push(answer);
+    askQuestion(questionIndex + 1);
+  });
+};
+
+
+const generateProfile = () => {
+  const name = answers[0][0].toUpperCase() + answers[0].slice(1);
+  const activity = answers[1];
+  const music = answers[2];
+  const meal = answers[3];
+  const favoriteFood = answers[4];
+  const sport = answers[5];
+  const superpower = answers[6];
+
+  console.log(`${name} loves to ${activity} while listening to ${music}, devouring ${favoriteFood} for ${meal}, prefers ${sport} over any other sport, and is amazing at ${superpower}.`)
+}
+
+askQuestion(0);
